0049ede15075866b57d21041169f0ec34ca6de47
sukwhan (david) <younsa@bc.edu>
2018-01-25 20:54:24 -0500

    1/25

--- ctci.js
+++ ctci.js
@@ -23,7 +23,47 @@ function URLify(input, num){
 
 // 1.4 Palindrome Permutation.
 
-//hello
-// testing
+function palindromePermutation(str){
+    var hash = {}
+    var numSpace = 0
+    for (let i = 0; i<str.length; i++){
+        if (str[i] === " "){
+            numSpace++
+            continue
+        }
+        else if (hash.hasOwnProperty(str[i])){
+            hash[str[i]]++
+        }
+        else{
+            hash[str[i]] = 1
+        }
+    }
+    console.log(hash)
+    var isEven = (str.length-numSpace)%2 === 0 ? true:false
+    var keys = Object.keys(hash)
+    console.log(keys)
+    // console.log(isEven)
+    if (isEven){ // all chars have to have even number.
+        for (let i = 0; i<keys.length; i++){
+            if (hash[keys[i]]%2 !== 0){
+                return false
+            }
+        }
+    }
+    else{ // if odd
+        var bool = false
+        for (let i = 0; i<keys.length; i++){
+            if (hash[keys[i]]%2 !== 0){
+                if (bool){
+                    return false
+                }
+                bool = true
+            }
+        }
+    }
+    return true
+}
 
-// 123
+// console.log(palindromePermutation("race eecar"))
+
+// 1.5 One Away
